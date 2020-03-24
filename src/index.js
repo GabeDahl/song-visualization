f13a4ed7435f21a7a7700ac7bde1329bc708112b
GabeDahl <45904434+GabeDahl@users.noreply.github.com>
2020-03-23 19:15:01 -0500

    stripped skeleton

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 
-import Layout from './components/Layout';
 
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
     <Provider store={store}>
-            <Layout />
+            
     </Provider>,
     document.getElementById('root')
 );
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import { getPlaylist } from './playlists'
-
-const rootReducer = combineReducers({
-    getPlaylist
-})
-
-export default rootReducer
