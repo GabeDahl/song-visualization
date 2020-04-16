5feefd5e966aa6b2d314e16303f1fc7a791fa6ff
GabeDahl <45904434+GabeDahl@users.noreply.github.com>
2020-04-16 08:29:55 -0500

    fix maybe

--- src/components/Chart/PitchSelect.jsx
+++ src/components/Chart/PitchSelect.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-
 import Checkbox from '@material-ui/core/Checkbox';
 import { FormGroup, FormControlLabel } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
-  
-}));
-
 export default function PitchSelect(props) {
-  const classes = useStyles();
-
   const pitchClasses = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'] 
-
   return (
       <FormGroup row='true'>
             {pitchClasses.map( (p) => {
